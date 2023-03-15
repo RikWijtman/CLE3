88a3ab86cd6a2b80117cd0ff578335704748edfa
sanderdb1509 <swdb2002@gmail.com>
2023-03-15 14:25:26 +0100

    tram page

--- tram.php
+++ tram.php
@@ -1,1 +1,27 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tram pagina</title>
+    <link rel="stylesheet" href="Tram style.css">
+</head>
+<body>
+<div class="container">
+    <div class="navbar">
+        <img src="logo.png" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="Metro"> METRO</a></li>
+                <li><a href="Trein">TREIN</a></li>
+                <li><a href="Bus">BUS</a></li>
+                <li><a href="Tram">TRAM</a></li>
+            </ul>
+        </nav>
+        <img src="menu.png" alt="menu-icon">
+    </div>
+    <iframe src="https://my.atlistmaps.com/map/75bcb9f5-6c8a-4a24-badb-691f6b3cb393?share=true" allow="geolocation" width="100%" height="800px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+</div>
+</body>
+</html>
