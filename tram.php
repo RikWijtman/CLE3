34d1ef78339167ae63f1150f1d1c87d50b46347c
sanderdb1509 <swdb2002@gmail.com>
2023-03-28 12:13:15 +0200

    tram page tips

--- tram.php
+++ tram.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tram pagina</title>
-    <link rel="stylesheet" href="Tram style.css">
+    <link rel="stylesheet" href="tramstyle.css">
     <script type="text/javascript" src="js/tram.js" defer></script>
 </head>
 <body>
