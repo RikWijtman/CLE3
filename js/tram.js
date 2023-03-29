b699038aec3782b35cd26f9dfb71bc65b9021001
sanderdb1509 <swdb2002@gmail.com>
2023-03-29 10:31:16 +0200

    tram page tips

--- js/tram.js
+++ js/tram.js
@@ -17,3 +17,25 @@ function addItem() {
         listActive = false;
     }
 }
+// Maak een Leaflet-kaart met de juiste coördinaten en zoomniveau
+var map = L.map('mapid').setView([51.9244201, 4.4777325], 13);
+
+// Voeg een basiskaart toe aan de kaart (bijvoorbeeld OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Tramhaltes data &copy; <a href="https://www.ret.nl/">RET</a>',
+    maxZoom: 18
+}).addTo(map);
+
+// Haal de gegevens van de tramhaltes op via de API van de RET
+fetch('https://api.ret.net/v1/lines/14/stops')
+    .then(response => response.json())
+    .then(data => {
+        // Verwerk de gegevens en voeg markers toe aan de kaart
+        data.forEach(stop => {
+            var marker = L.marker([stop.location.latitude, stop.location.longitude]).addTo(map);
+            marker.bindPopup(stop.name);
+        })
+    });
+
