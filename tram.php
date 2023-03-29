6a7bb7dc116fded02f4bea0ed5cf9379fb82b06e
sanderdb1509 <swdb2002@gmail.com>
2023-03-29 10:02:02 +0200

    tram page tips

--- tram.php
+++ tram.php
@@ -27,9 +27,6 @@
         <div id="list">
         </div>
         </div>
-    <iframe src="https://my.atlistmaps.com/map/75bcb9f5-6c8a-4a24-badb-691f6b3cb393?share=true" allow="geolocation" width="100%" height="500px" frameborder="0" scrolling="no" allowfullscreen></iframe>
-</div>
-
 
 </body>
 </html>
