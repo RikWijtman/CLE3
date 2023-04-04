4ee569bb302d818e6a257513cc97806fcf69c1f1
slveltman <0992816@hr.nl>
2023-04-04 12:20:46 +0200

    data metro

--- metro/metro.php
+++ metro/metro.php
@@ -12,5 +12,19 @@
 <body>
 <button id="tips">Toon tips</button>
 <ul id="list"></ul>
+<form id="form">
+    <div class="form-item">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name"/>
+    </div>
+    <div class="form-item">
+        <label for="age">Age</label>
+        <input type="number" id="age" name="age"/>
+    </div>
+    <div class="form-item">
+        <input type="submit" name="submit" value="Save in local storage"/>
+    </div>
+</form>
+<button type="button" id="delete" >Delete items in local storage</button>
 </body>
 </html>
