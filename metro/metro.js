83be0f064c6cc1bfcc429c751ca0f5d5429197f3
slveltman <0992816@hr.nl>
2023-05-23 13:31:27 +0200

    data metro 3

--- metro/metro.js
+++ metro/metro.js
@@ -12,126 +12,32 @@ let listActive = false;
             for (let i = 0; i <= 3; i++) {
                 let tip = data.tips;
                 let tipItem = document.createElement('li');
-                tipItem.textContent = tip[i].text;
+                tipItem.textContent = tip[i];
                 list.appendChild(tipItem);
             }
         })
 }
 window.addEventListener('load', init);
 
-//Global vars
-let form;
-let nameField;
-let ageField;
-let deleteButton;
+// Click event handler
+let showObjectsButton = document.getElementById("showObjectsButton");
+showObjectsButton.addEventListener("click", createObjects);
 
-function init() {
-    if (typeof window.localStorage === "undefined") {
-        console.error('Local storage is not available in your browser');
-        return;
-    }
+function createObjects() {
+    // Gegevens ophalen van de PHP-server
+    fetch('server.php')
+        .then(response => response.json())
+        .then(data => {
+            let objectContainer = document.getElementById("objectContainer");
 
-    form = document.getElementById('form');
-    nameField = document.getElementById('name');
-    deleteButton = document.getElementById('delete');
-
-    fillFieldsFromLocalStorage();
-    form.addEventListener('submit', formSubmitHandler);
-    deleteButton.addEventListener('click', deleteClickHandler);
-}
-
-/**
- * Is local storage is available on page load? Let's fill the form
- */
-function fillFieldsFromLocalStorage() {
-    if (localStorage.getItem('name') !== null) {
-        nameField.value = localStorage.getItem('name');
-    }
-}
-
-/**
- * After submitting the form, let's save the values in the local storage
- *
- * @param e
- */
-function formSubmitHandler(e) {
-    e.preventDefault();
-    localStorage.setItem('name', nameField.value);
-}
-
-/**
- * Make sure we clean up the local storage again
- *
- * @param e
- */
-function deleteClickHandler(e) {
-    localStorage.removeItem('name');
-}
-
-
-// Selecteer alle elementen met class naam 'circle'
-const circles = document.querySelectorAll('.circle');
-
-// Loop door alle gevonden elementen heen
-circles.forEach(circle => {
-    // Maak een nieuwe button
-    const button2 = document.createElement('button');
-
-    // Geef de button tekst
-    button2.innerText = '';
-
-    // Voeg een click event listener toe aan de button
-    button2.addEventListener('click', function() {
-        console.log("top");
-    });
-
-    // Vervang de circle met de button
-    circle.parentNode.replaceChild(button2, circle);
-
-    // Voeg de class 'circle' toe aan de button
-    button2.classList.add('circle');
-});
-
-// Haal het JSON-bestand op met behulp van een HTTP-request
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        // Haal de gewenste gegevens uit het JSON-object
-        const stationName = data.stationname;
-        const placeName = data.placename;
-        const todo = data.todo;
-        const accessibility = data.accessibility;
-        const rush = data.rush;
-
-        // Doe iets met de gegevens, bijvoorbeeld loggen naar de console
-        console.log(data)
-
-    })
-    .catch(error => {
-        // Als er een fout optreedt bij het ophalen of parsen van de JSON
-        console.error('Error fetching JSON file:', error);
-    });
-
-fetch(`server.php`)
-    .then(response => response.json())
-    .then(data => {
-        for (let i = 1; i <= 10; i++) {
-            const circles = document.querySelectorAll(".circle" + i);
-            circles.forEach(circle => {
-                console.log(data);
+            // Loop door de gegevens en maak objecten
+            Object.values(data).forEach(station => {
+                let newObject = document.createElement("div");
+                newObject.textContent = "Stationname: " + station.stationname;
+                objectContainer.appendChild(newObject);
             });
-        }
-
-        // Doe iets met de gegevens, bijvoorbeeld loggen naar de console
-        console.log(data)
-
-    })
-    .catch(error => {
-        // Als er een fout optreedt bij het ophalen of parsen van de JSON
-        console.error('Error fetching JSON file:', error);
-    });
-
-
-
-
-
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
--- metro/metro.php
+++ metro/metro.php
@@ -11,75 +11,11 @@
 </head>
 <body>
 <br>
-<nav class="subway row justify-around align-end">
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Hoek van Holland</span>
-        <div class="circle z-15" data-id="1"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Schiedam Centrum</span>
-        <div class="circle" data-id="2"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Dijkzigt</span>
-        <div class="circle" data-id="3"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>beurs</span>
-        <div class="circle" data-id="4"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Blaak</span>
-        <div class="circle" data-id="5"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Oostplein</span>
-        <div class="circle" data-id="6"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Kralingsezoom</span>
-        <div class="circle" data-id="7"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Capelsebrug</span>
-        <div class="circle" data-id="8"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Alexander</span>
-        <div class="circle" data-id="9"></div>
-    </div>
-    <div class="item column align-center">
-        <hr class="line" />
-        <span>Nesselande</span>
-        <div class="circle" data-id="10"></div>
-    </div>
-
-
-
 </nav>
 <button id="tips">Toon tips</button>
 <ul id="list"></ul>
-<form id="form">
-    <div class="form-item">
-        <label for="name">jou veel gebruikte station </label>
-        <input type="text" id="name" name="name"/>
-    </div>
-    <div class="form-item">
-        <input type="submit" name="submit" value="Save in local storage"/>
-    </div>
-</form>
-<button type="button" id="delete" >Delete items in local storage</button>
 <br>
-
-
+<button id="showObjectsButton">Toon Objecten</button>
+<div id="objectContainer"></div>
 </body>
 </html>
